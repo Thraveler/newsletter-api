90a05a916f8700dac49dce3feb942620d945c5df
Daniel Méndez <daphni_mgld@hotmail.com>
2023-03-30 00:30:31 -0600

    feat: Aut h routes improved with better responses and handling e rrors

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,15 +2,47 @@ import { Request, Response } from "express";
 import * as AuthService from "../services/auth.service";
 
 const register = async (req: Request, res: Response) => {
-  const userCreated = await AuthService.createUser(req.body);
+  try {
+    const userCreated = await AuthService.createUser(req.body);
 
-  res.status(201).json(userCreated);
+    if (!userCreated) {
+      res.status(500).json({
+        error: "There was an error while creating an account",
+      });
+    } else {
+      res.status(201).json({
+        message: "User created successfully",
+      });
+    }
+  } catch (error) {
+    console.error("There was an error while creating an account", error);
+
+    res.status(500).json({
+      error: "There was an error while creating an account",
+    });
+  }
 };
 
 const login = async (req: Request, res: Response) => {
-  const isLogged = await AuthService.loginUser(req.body);
+  try {
+    const isLogged = await AuthService.loginUser(req.body);
 
-  res.json(isLogged);
+    if (!isLogged) {
+      res.status(404).json({
+        message: "Can't login, validate your credentials",
+      });
+    } else {
+      res.status(200).json({
+        token: isLogged,
+      });
+    }
+  } catch (error) {
+    console.error("There was an error while login", error);
+
+    res.status(500).json({
+      error: "There was an error while login",
+    });
+  }
 };
 
 export { register, login };
