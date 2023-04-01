b2cafc3f112038fa189fcb00e2a1969d5ed12853
Daniel Méndez <daphni_mgld@hotmail.com>
2023-04-01 02:39:44 -0600

    fead: cors added to be able to fetch data from front

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express, { Request, Response } from "express";
 import "dotenv/config";
 import router from "./routes";
 import db from "./config/database";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
