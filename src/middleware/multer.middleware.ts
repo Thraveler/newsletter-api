fa14f6f1d8011d6c2f1330ddb7425c7bb36d26a2
Daniel Méndez <daphni_mgld@hotmail.com>
2023-04-03 03:04:34 -0600

    feat: Restrict to only upload images and pdf files on campaign

--- src/middleware/multer.middleware.ts
+++ src/middleware/multer.middleware.ts
@@ -2,6 +2,12 @@ import { Request } from "express";
 import multer, { diskStorage } from "multer";
 
 const PATH_STORAGE = `${process.cwd()}/uploads`;
+const whitelist: string[] = [
+  'image/png',
+  'image/jpeg',
+  'image/jpg',
+  'application/pdf'
+]
 
 const storage = diskStorage({
   destination(req: Request, file: Express.Multer.File, cb: any) {
@@ -9,11 +15,24 @@ const storage = diskStorage({
   },
   filename(req: Request, file: Express.Multer.File, cb: any) {
     const ext = file.originalname.split(".").pop();
-    const fileNameRandom = `image-${file.fieldname}-${Date.now()}.${ext}`;
-    cb(null, fileNameRandom);
+    const name = file.originalname.split(".").shift();
+    const newFileName = `${name}-${Date.now()}.${ext}`;
+    cb(null, newFileName);
   },
 });
 
-const multerMiddleware = multer({ storage });
+const multerMiddleware = multer({
+  storage, fileFilter: (req: Request, file: Express.Multer.File, cb: any) => {
+    if (whitelist.includes(file.mimetype)) {
+      console.log(whitelist.includes(file.mimetype))
+      cb(null, true);
+    } else {
+
+      console.log(whitelist.includes(file.mimetype))
+      cb(null, false);
+      return cb(new Error('Only .png, .jpg, .jpeg  and .pdf format allowed!'));
+    }
+  }
+});
 
 export default multerMiddleware;
